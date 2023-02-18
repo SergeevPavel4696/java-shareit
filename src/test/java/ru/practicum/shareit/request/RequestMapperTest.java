5cd6dd1a0704254dd59b57543f3f9e1b7d61b25b
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2023-02-18 13:57:41 +0300

    Add files via upload

---
+++ src/test/java/ru/practicum/shareit/request/RequestMapperTest.java
@@ -0,0 +1,69 @@
+package ru.practicum.shareit.request;
+
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.item.model.ItemDto;
+import ru.practicum.shareit.request.mapper.ItemRequestMapper;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.request.model.ItemRequestDto;
+import ru.practicum.shareit.request.model.ItemRequestDtoResponse;
+import ru.practicum.shareit.request.model.ItemRequestDtoWithItems;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RequestMapperTest {
+
+    private final int userId = 1;
+    private final String userName = "Имя";
+    private final String email = "mail@mail.ya";
+    private final User requester = new User(userId, userName, email);
+
+    private final int requestId = 2;
+    private final String description = "Описание";
+    private final LocalDateTime created = LocalDateTime.of(2020, 10, 15, 12, 40, 35);
+    private final ItemRequest itemRequest = new ItemRequest(requestId, description, requester, created);
+    private final ItemRequestDto itemRequestDto = new ItemRequestDto(requestId, description);
+
+    private final List<ItemDto> items = List.of(
+            new ItemDto(1, "Название 1", "Описание 1", true, null),
+            new ItemDto(2, "Название 2", "Описание 2", false, 1),
+            new ItemDto(3, "Название 3", "Описание 3", true, 2)
+    );
+
+    @Test
+    public void ItemRequestToItemRequestDtoTest() {
+        ItemRequestDto itemRequestDto = ItemRequestMapper.convert(itemRequest);
+        assertEquals(requestId, itemRequestDto.getId());
+        assertEquals(description, itemRequestDto.getDescription());
+    }
+
+    @Test
+    public void ItemRequestDtoToItemRequestTest() {
+        ItemRequest itemRequest = ItemRequestMapper.convert(itemRequestDto, requester, created);
+        assertEquals(requestId, itemRequest.getId());
+        assertEquals(description, itemRequest.getDescription());
+        assertEquals(requester.getId(), itemRequest.getRequester().getId());
+        assertEquals(created, itemRequest.getCreated());
+    }
+
+    @Test
+    public void ItemRequestToItemRequestDtoResponseTest() {
+        ItemRequestDtoResponse itemRequestDtoResponse = ItemRequestMapper.convertToResponse(itemRequest);
+        assertEquals(requestId, itemRequestDtoResponse.getId());
+        assertEquals(description, itemRequestDtoResponse.getDescription());
+        assertEquals(created, itemRequestDtoResponse.getCreated());
+    }
+
+    @Test
+    public void ItemRequestToItemRequestDtoWithItemsTest() {
+        ItemRequestDtoWithItems itemRequestDtoWithItems = ItemRequestMapper.convert(itemRequest, new ArrayList<>(items));
+        assertEquals(requestId, itemRequestDtoWithItems.getId());
+        assertEquals(description, itemRequestDtoWithItems.getDescription());
+        assertEquals(created, itemRequestDtoWithItems.getCreated());
+        assertEquals(items, itemRequestDtoWithItems.getItems());
+    }
+}
---
+++ src/test/java/ru/practicum/shareit/user/UserMapperTest.java
@@ -0,0 +1,34 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.user.mapper.UserMapper;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.model.UserDto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserMapperTest {
+
+    private final int id = 1;
+    private final String name = "Имя";
+    private final String email = "mail@mail.ya";
+
+    private final User user = new User(id, name, email);
+    private final UserDto userDto = new UserDto(id, name, email);
+
+    @Test
+    public void UserToUserDtoTest() {
+        UserDto userDto = UserMapper.convert(user);
+        assertEquals(id, userDto.getId());
+        assertEquals(name, userDto.getName());
+        assertEquals(email, userDto.getEmail());
+    }
+
+    @Test
+    public void UserDtoToUserTest() {
+        User user = UserMapper.convert(userDto);
+        assertEquals(id, user.getId());
+        assertEquals(name, user.getName());
+        assertEquals(email, user.getEmail());
+    }
+}
